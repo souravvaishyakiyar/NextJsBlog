414f6fcb6071c3fbd05fa108929653087eeac62a
Sourav <souravvaishyakiyar@gmail.com>
2025-04-07 02:09:39 +0530

    NextJs setup and model setup

---
+++ blogapp/src/app/(home)/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/components/home/navbar";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+    <Navbar/>
+    </div>
+  );
+}
